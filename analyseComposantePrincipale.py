e0fd8e8171470c851cecc8b4da8d87cb01561f60
ycbkaii <yanis.chiouar@gmail.com>
2024-10-01 14:45:14 +0200

    fin acp

--- analyseComposantePrincipale.py
+++ analyseComposantePrincipale.py
@@ -5,6 +5,7 @@ import seaborn
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from biplot import biplot
+import random as rd
 
 
 # On récupere le fichier csv et on nettoie ce dernier
@@ -14,8 +15,23 @@ data = pd.read_csv("csv/bigboss_book.csv")
 # On récupere les colonnes quantitatives
 variables = ['rating_count', 'review_count', 'average_rating', 'five_star_ratings', 'four_star_ratings', 'three_star_ratings', 'two_star_ratings', 'one_star_ratings', 'number_of_pages']
 
-# Pour le graphique des individus on va récupérer une variable qualitative 'author'
-variableQualitative = data['author']
+# Pour le graphique des individus on va récupérer une variable qualitative 'date_published'
+variableQualitative = data['date_published'].astype(str)
+
+# On créé une fonction pour qualitativiser la variable
+def datePublished(date) :
+    dateRetenu = date.split()[-1]
+    if dateRetenu == None or dateRetenu == 'nan' : 
+        return 'Date Inconnu'
+
+    if int(dateRetenu) >= 2010 :
+        return 'Livre Très Récent'
+    elif int(dateRetenu) < 2010 and int(dateRetenu) >= 2000 : 
+        return 'Livre Récent'
+    else :
+        return 'Ancien Livre'
+
+variableQualitative = variableQualitative.apply(datePublished)
 
 # On nettoie et on garde que les données qu'on va utiliser
 data = data[variables]
@@ -81,20 +97,25 @@ plt.show()
 pca_df = pd.DataFrame({
     "Dim1" :  pca_res[:,0],
     "Dim2" :  pca_res[:,1],
-    "cat_but" : variableQualitative.to_numpy()
+    "cat_date" : variableQualitative.to_numpy()
 })
+
+
 
 #TODO Finir ACP
 
+
 # La pallette de couleurs
 palette = plt.get_cmap('Dark2')
-couleurs = dict(zip(pca_df["cat_but"].drop_duplicates(),palette(range(10))))
-position = dict(zip(couleurs.keys(),range(10))) 
+couleurs = dict(zip(pca_df["cat_date"].drop_duplicates(),palette(range(len(pca_df['cat_date'].drop_duplicates())))))
+position = dict(zip(couleurs.keys(),range(len(pca_df['cat_date'].drop_duplicates())))) 
 
-pca_df.plot.scatter("Dim1", "Dim2", c=[couleurs[p] for p in pca_df["cat_but"]])
+
+
+pca_df.plot.scatter("Dim1", "Dim2", c=[couleurs[p] for p in pca_df["cat_date"]])
 for cont, coul in couleurs.items():
-    plt.scatter(11, position[cont] / 2 + 2.15, c=[coul])
-    plt.text(11.1, position[cont] / 2 + 2, cont)
+    plt.scatter(135, position[cont] / 2 + 2.15, c=[coul])
+    plt.text(136, position[cont] / 2 + 2, cont)
 plt.xlabel("Dimension 1 (%)")
 plt.ylabel("Dimension 2 (%)")
 plt.suptitle("Premier plan factoriel (%)")
