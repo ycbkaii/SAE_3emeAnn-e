a1277cae0c58728d89ed74e9f22293dcfa4a96fd
Guillaume BARON <guillaume.baron62@orange.fr>
2024-10-02 10:54:36 +0200

    2 Pie chart sur le meme + pourcentage + légende bien

--- pieChart.py
+++ pieChart.py
@@ -50,12 +50,12 @@ data_genre_bien = (
     .drop(0, axis=1)  # On supprime la colonne avec les ancien genre
 )
 
-data_genre_principale = data_genre_bien.iloc[:26]
-data_autre_genre = data_genre_bien.iloc[26:]
+data_genre_principale = data_genre_bien.iloc[:9]
+data_autre_genre = data_genre_bien.iloc[9:]
 count_autre = data_autre_genre.sum(numeric_only=True).to_list()[0]
 
 df_autre = pd.DataFrame(
-    {"genre_grp": "Autre", "count": count_autre}, index=[1]
+    {"genre_grp": "Other (less than 3.4 percent)", "count": count_autre}, index=[1]
 )  # On crée la ligne "autre" dans un nouveau dataFrame
 
 
@@ -68,14 +68,46 @@ labels = data_genre_principale["genre_grp"]
 labels_autre = data_autre_genre["genre_grp"]
 
 
-plt.pie(
-    data_genre_principale["count"].to_numpy(),
-    labels=labels.to_list(),
-    startangle=90,
+fig, (ax1, ax2) = plt.subplots(
+    1, 2, figsize=(100, 100), subplot_kw=dict(aspect="equal")
 )
-plt.title("Test pie chart")
-plt.show()
 
-plt.pie(data_autre_genre["count"].to_numpy(), labels=labels_autre, startangle=90)
-plt.title("Autre pie chart")
-plt.show()
+explode =([0] * 9) + [0.1]
+wedges, texts, autotexts = ax1.pie(
+    data_genre_principale["count"].to_numpy(),
+    startangle=90,
+    autopct="%1.2f%%",
+    radius=1.2,
+    explode=explode
+)
+
+# TODO voir si on peut mettre les pourcentage sur la légende
+
+ax1.legend(
+    wedges,
+    labels.to_list(),
+    title="Gender",
+    loc="upper left",
+    bbox_to_anchor=(1, 0, 0.5, 1),
+)
+
+plt.setp(autotexts, size=10)
+
+ax1.set_title("Principal gender pie chart")
+
+wedges2, texts2, autotexts2 = ax2.pie(
+    data_autre_genre["count"].to_numpy(),
+    startangle=90,
+    autopct="%1.1f%%",
+)
+
+ax2.legend(
+    wedges2,
+    labels_autre.to_list(),
+    title="Gender",
+    loc="center left",
+    bbox_to_anchor=(1, 0, 0.5, 1),
+)
+
+ax2.set_title("Other gender pie chart")
+#plt.show()
