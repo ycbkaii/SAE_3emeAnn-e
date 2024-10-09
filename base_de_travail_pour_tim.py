9c4370d473d885edb60a55347e938d9d8c8320e1
tom27012004 <127438871+tom27012004@users.noreply.github.com>
2024-10-09 14:02:34 +0200

    traduction en anglais du code

--- base_de_travail_pour_tim.py
+++ base_de_travail_pour_tim.py
@@ -2,59 +2,60 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-# On récupere le fichier csv et on nettoie ce dernier
+# Loading the CSV file and cleaning it
 data = pd.read_csv("csv/bigboss_book.csv")
 
-variables = ['author', 'rating_count', 'review_count', 'average_rating','genre_and_votes', 'series', 'five_star_ratings', 'four_star_ratings', 'three_star_ratings', 'two_star_ratings', 'one_star_ratings', 'number_of_pages', 'publisher', 'date_published']
 
-# On nettoie et on garde que les données qu'on va utiliser
+variables = ['author', 'rating_count', 'review_count', 'average_rating', 'genre_and_votes', 'series', 'five_star_ratings', 'four_star_ratings', 'three_star_ratings', 'two_star_ratings', 'one_star_ratings', 'number_of_pages', 'publisher', 'date_published']
+
+# Cleaning and keeping only the data we will use
 data = data[variables]
 
 print(data)
 
-# On peut commencer les analyses
+# We can start the analysis
 
 
-# Sélection des colonnes de notation
+# Selecting the rating columns
 ratings_columns = ['five_star_ratings', 'four_star_ratings', 'three_star_ratings', 'two_star_ratings', 'one_star_ratings']
 
-# Calcul des moyennes et des écarts-types des notations par catégorie d'étoiles
+# Calculating the means and standard deviations of the ratings by star category
 mean_values = data[ratings_columns].mean()
 std_values = data[ratings_columns].std()
 
-# Taille de la population (ici, on suppose n = nombre de livres dans chaque catégorie)
+# Population size (here, we assume n = number of books in each category)
 n = len(data)
 
-# Calcul de l'intervalle de confiance à 95 %
+# Calculating the 95% confidence interval
 ci = 1.96 * std_values / np.sqrt(n)
 
-# Création du graphique
+# Creating the chart
 plt.figure()
-x = np.array([5, 4, 3, 2, 1])  # Les catégories d'étoiles
+x = np.array([5, 4, 3, 2, 1])  # Star categories
 y = mean_values.values
 
-plt.plot(x, y, marker='o', linestyle='-', color='b', label='Moyenne des notes')
-plt.fill_between(x, (y - ci), (y + ci), color='b', alpha=0.1, label='Intervalle de confiance à 95%')
+plt.plot(x, y, marker='o', linestyle='-', color='b', label='Average Ratings')
+plt.fill_between(x, (y - ci), (y + ci), color='b', alpha=0.1, label='95% Confidence Interval')
 
-# Personnalisation du graphique
-plt.xlabel('Nombre d\'étoiles')
-plt.ylabel('Nombre moyen de notes')
-plt.title('Nombre moyen de notes par étoiles avec intervalle de confiance à 95%')
-plt.xticks([5, 4, 3, 2, 1], ['5 étoiles', '4 étoiles', '3 étoiles', '2 étoiles', '1 étoile'])
+# Customizing the chart
+plt.xlabel('Number of Stars')
+plt.ylabel('Average Number of Ratings')
+plt.title('Average Number of Ratings by Star Category with 95% Confidence Interval')
+plt.xticks([5, 4, 3, 2, 1], ['5 stars', '4 stars', '3 stars', '2 stars', '1 star'])
 plt.legend()
 plt.grid(True)
 
-# Affichage du graphique
+# Displaying the chart
 plt.show()
 
-# Partie 2 : Box Plot
 
-# Création du box plot
+# Part 2: Box Plot
+
+# Creating the box plot
 plt.figure(figsize=(10, 5))
 data[ratings_columns].plot.box()
-plt.title("Box Plot des notations en fonction des étoiles")
-plt.ylabel("Nombre de notes")
-plt.xticks([1, 2, 3, 4, 5], ['5 étoiles', '4 étoiles', '3 étoiles', '2 étoiles', '1 étoile'])
+plt.title("Box Plot of Ratings by Star Category")
+plt.ylabel("Number of Ratings")
+plt.xticks([1, 2, 3, 4, 5], ['5 stars', '4 stars', '3 stars', '2 stars', '1 star'])
 plt.grid(True)
 plt.show()
