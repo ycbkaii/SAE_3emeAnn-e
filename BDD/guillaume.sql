077de326d1743bd56203f1902dfb4e55faf48b94
Guillaume BARON <guillaume.baron62@orange.fr>
2024-11-12 08:27:55 +0100

    les vues

--- BDD/guillaume.sql
+++ BDD/guillaume.sql
@@ -1,8 +1,10 @@
 SET SCHEMA 'masterbook';
 
+-- On drop les tables pour être sur qie cela mets bien à jour
+
 -- Table Livre
-CREATE TABLE "Livre" (
-	"id" serial NOT NULL UNIQUE,
+CREATE TABLE "_livre" (
+	"id_livre" serial NOT NULL UNIQUE,
 	"title" varchar(255) NOT NULL,
 	"description" varchar(255),
 	"number_of_page" int,
@@ -15,41 +17,41 @@ CREATE TABLE "Livre" (
 
 
 -- Table Genre
-CREATE TABLE "genres" (
+CREATE TABLE "_genre" (
 	"id_genre" serial NOT NULL UNIQUE,
 	"nom_genre" varchar(255),
 	PRIMARY KEY("id_genre")
 );
 
 -- Table Personnage
-CREATE TABLE "Personnage" (
+CREATE TABLE "_personnage" (
 	"id_personnage" serial NOT NULL UNIQUE,
 	"Nom_personnage" varchar(100),
 	PRIMARY KEY("id_personnage")
 );
 
 -- Table Possede_personnage
-CREATE TABLE "possede_personnage" (
+CREATE TABLE "_possede_personnage" (
 	"id_books" int NOT NULL UNIQUE,
 	"id_personnage" int NOT NULL,
 	PRIMARY KEY("id_books", "id_personnage")
 );
 
 -- Table Publisher
-CREATE TABLE "Publisher" (
+CREATE TABLE "_publisher" (
 	"id_publisher" serial NOT NULL UNIQUE,
 	PRIMARY KEY("id_publisher")
 );
 
 -- Table a_publie
-CREATE TABLE "a_publie" (
+CREATE TABLE "_a_publie" (
 	"id_publisher" int NOT NULL UNIQUE,
 	"id_book" int NOT NULL,
 	PRIMARY KEY("id_publisher", "id_book")
 );
 
 -- Table Genre_du_livre
-CREATE TABLE "genres_du_livre" (
+CREATE TABLE "_genres_du_livre" (
 	"id_genre" int NOT NULL UNIQUE,
 	"nombre_votes_utilisateur" int,
 	"id_livre" int NOT NULL UNIQUE,
@@ -60,12 +62,12 @@ CREATE TABLE "genres_du_livre" (
 -- Clef étrangère table possede_personnage
 ------------------------------------------
 
-ALTER TABLE "possede_personnage"
-ADD FOREIGN KEY("id_books") REFERENCES "Livre"("id")
-ON UPDATE CASCADE ON DELETE NO ACTION;
+ALTER TABLE "_possede_personnage"
+ADD FOREIGN KEY("id_books") REFERENCES "_livre"("id_livre")
+ON UPDATE CASCADE ON DELETE CASCADE;
 
-ALTER TABLE "possede_personnage"
-ADD FOREIGN KEY("id_personnage") REFERENCES "Personnage"("id_personnage")
+ALTER TABLE "_possede_personnage"
+ADD FOREIGN KEY("id_personnage") REFERENCES "_personnage"("id_personnage")
 ON UPDATE CASCADE ON DELETE CASCADE;
 
 
@@ -73,20 +75,43 @@ ON UPDATE CASCADE ON DELETE CASCADE;
 -- Clef étrangère table a_publie
 ------------------------------------------
 
-ALTER TABLE "a_publie"
-ADD FOREIGN KEY("id_publisher") REFERENCES "Publisher"("id_publisher")
+ALTER TABLE "_a_publie"
+ADD FOREIGN KEY("id_publisher") REFERENCES "_publisher"("id_publisher")
 ON UPDATE CASCADE ON DELETE CASCADE;
-ALTER TABLE "a_publie"
-ADD FOREIGN KEY("id_book") REFERENCES "Livre"("id")
+ALTER TABLE "_a_publie"
+ADD FOREIGN KEY("id_book") REFERENCES "_livre"("id_livre")
 ON UPDATE CASCADE ON DELETE CASCADE;
 
 ------------------------------------------
 -- Clef étrangère table genres_du_livre
 ------------------------------------------
 
-ALTER TABLE "genres_du_livre"
-ADD FOREIGN KEY("id_genre") REFERENCES "genres"("id_genre")
+ALTER TABLE "_genres_du_livre"
+ADD FOREIGN KEY("id_genre") REFERENCES "_genre"("id_genre")
 ON UPDATE CASCADE ON DELETE CASCADE;
-ALTER TABLE "genres_du_livre"
-ADD FOREIGN KEY("id_livre") REFERENCES "Livre"("id")
+ALTER TABLE "_genres_du_livre"
+ADD FOREIGN KEY("id_livre") REFERENCES "_livre"("id_livre")
 ON UPDATE CASCADE ON DELETE CASCADE;
+
+------------------------------------------
+-- Vue pour les valeurs calculées 
+------------------------------------------
+
+CREATE VIEW "rating_livres" AS
+SELECT id_livre, AVG(note_livre) as average_rating, COUNT(note_livre) as rating_count
+FROM a_lu_livre_vote_genre_pour_livre
+GROUP BY id_livre
+WHERE note_livre IS NOT NULL
+;
+
+CREATE VIEW "review_livres" AS
+SELECT id_livre, COUNT(review) as review_count
+FROM a_lu_livre_vote_genre_pour_livre
+GROUP BY id_livre
+WHERE review IS NOT NULL
+;
+
+CREATE VIEW "livres_calculees" AS
+SELECT review_livres.id_livre, average_rating, rating_count, review_count
+FROM review_livres
+NATURAL JOIN rating_livres;
