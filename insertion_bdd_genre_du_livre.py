ae689695e6c1c88549aac062caacbb1e5afdc031
Guillaume Baron <guillaume.baron62@orange.fr>
2024-11-12 19:47:26 +0100

    genre_du_livre

---
+++ insertion_bdd_genre_du_livre.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+raw_csv = pd.read_csv("csv/bigboss_book.csv")
+
+data_extract = raw_csv[["id","genre_and_votes"]]
+
+# On drop les livres ou ya pas de genres
+data_extract = data_extract.dropna()
+
+# Function to split genre and votes
+def split_genres(row):
+    genres = row['genre_and_votes'].split(', ')
+    df_list = []
+    for genre in genres:
+        parts = genre.split()
+        new_id = row['id']
+        new_genre = ' '.join(parts[:-1]) 
+        new_votes = 1 if parts[-1] == "1user" else int(parts[-1])      # Last element is votes + cas spécifique de 1user
+        df_list.append({'id': new_id, 'genre': new_genre, 'votes': new_votes})
+    return pd.DataFrame(df_list)
+
+# Apply the function to all rows and concatenate results
+all_new_rows = pd.concat([split_genres(row) for index, row in data_extract.iterrows()], ignore_index=True)
+
+all_new_rows.to_csv("csv/peuplement_genre_du_livre.csv",header=False)
