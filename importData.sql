c75f3371eed24c936832959c28392ce27ed4a018
Guillaume BARON <guillaume.baron62@orange.fr>
2024-12-12 11:50:58 +0100

    test word2Vec et glove

--- importData.sql
+++
@@ -1,9 +0,0 @@
-COPY masterbook._genre(id_genre, nom_genre)
-FROM 'csv/peuplement_genre_livre.csv'
-DELIMITER ','
-CSV HEADER;
-
-COPY masterbook._genres_du_livre(id_livre,id_genre,nombre_votes_utilisateur)
-FROM 'csv/peuplement_genre_livre.csv'
-DELIMITER ','
-CSV HEADER;
