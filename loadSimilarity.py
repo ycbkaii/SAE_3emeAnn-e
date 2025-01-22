a10bcd2bf75497f3efef8194f9973eb17b4ebd06
Guillaume BARON <guillaume.baron62@orange.fr>
2025-01-22 14:20:37 +0100

    On peut save la matrice de cosinus

--- loadSimilarity.py
+++ loadSimilarity.py
@@ -1,8 +1,12 @@
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity,euclidean_distances
+import datetime as dt
 
-print(cosine_similarity(pd.read_csv("./vectGenre.csv").to_numpy())[204][100])
+start = dt.datetime.now()
+# print(cosine_similarity(pd.read_csv("./vectGenre.csv",index_col="id_genre").to_numpy())[-1][-2])
 print("---------------------------------------------------------------")
 print(euclidean_distances(pd.read_csv("./vectGenre.csv").to_numpy())[204][100])
 print("---------------------------------------------------------------")
-# print(pd.read_csv("./cosineSimGenre.csv").to_numpy()[-1])
+print(pd.read_csv("./cosineSimGenre.csv",index_col="id_genre").to_numpy()[2])
+end = dt.datetime.now()
+print((end - start).total_seconds())
--- ollama_emb.py
+++ ollama_emb.py
@@ -39,12 +39,25 @@ def embedDescAll() :
 
 def embedGenreAll() :
     listVecGenre = []
-    for gen in genre :
-        listVecGenre.append(embedText(str(gen))["embeddings"][0])
+    def processFils(start : bool) :
+        if start :
+            for gen in genre[:400] :
+                listVecGenre.append(embedText(str(gen))["embeddings"][0])
+        else :
+            for gen in genre[400:] :
+                listVecGenre.append(embedText(str(gen))["embeddings"][0])
+    thread1 = threading.Thread(target=processFils,args=[True],name="Tread pour le début")
+    thread2 = threading.Thread(target=processFils,args=[False],name="Tread pour la fin")
+    
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    thread2.join()
+
     return listVecGenre
 
 embedGenre = embedGenreAll()
-pd.DataFrame(embedGenre).to_csv("./vectGenre.csv")
+pd.DataFrame(embedGenre).to_csv("./vectGenre.csv",index_label="id_genre")
 pd.DataFrame(cosine_similarity(embedGenre)).to_csv("./cosineSimGenre.csv",index_label="id_genre")
 end = dt.datetime.now()
 print((end - start).total_seconds())
