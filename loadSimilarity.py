2cf31aef92d22f54274ca22cb90df44823006b03
Guillaume Baron <guillaume.baron62@orange.fr>
2025-01-25 18:59:23 +0100

    Prends les donnée de ES

--- loadSimilarity.py
+++ loadSimilarity.py
@@ -1,15 +1,17 @@
+from elasticsearch import Elasticsearch
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
+
 cosSimGenreBooks =  cosine_similarity(pd.read_csv("./vectGenre.csv",index_col="id_genre").to_numpy())
 # print("---------------------------------------------------------------")
-vectDescBooks =  pd.read_feather("./vectDesc1024")
-print(vectDescBooks)
-vectDescBooks = vectDescBooks.to_numpy()
-def calcSim2Books(id_books1,id_books2 ) :
-    livre1 : np.ndarray = vectDescBooks[id_books1]
-    livre2 : np.ndarray = vectDescBooks[id_books2]
-    return cosine_similarity(livre1.reshape(1,-1),livre2.reshape(1,-1))
 
-#print(calcSim2Books(42708,43277))
+client = Elasticsearch("http://localhost:9200")
+index_name = "embeddings-books"
+def calcSim2Books(id_books1,id_books2 ) :
+    livre1 = client.get(index=index_name,id=id_books1)["_source"]["description_vector"]
+    livre2  = client.get(index=index_name,id=id_books2)["_source"]["description_vector"]
+    return cosine_similarity(np.array(livre1).reshape(1,-1), np.array(livre2).reshape(1,-1))
+
+print(calcSim2Books(42708,43277))
