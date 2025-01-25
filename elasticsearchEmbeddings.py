2ae6c4265b31659c1fd98a0ece0072577c4bd429
Guillaume Baron <guillaume.baron62@orange.fr>
2025-01-25 18:59:09 +0100

    fct pour mettre les vects de genre dans ES

--- elasticsearchEmbeddings.py
+++ elasticsearchEmbeddings.py
@@ -3,36 +3,45 @@ import pandas as pd
 
 client = Elasticsearch("http://localhost:9200")
 
-client.info()
-
-vectDescBooks =  pd.read_feather("./vectDesc1024")
+vectDescBooks = pd.read_feather("./vectDesc1024")
 print(vectDescBooks)
 vectDescBooks = vectDescBooks.to_numpy()
 
-mappings = {
-      "properties": {
-        "description_vector": {
-          "type": "dense_vector",
-          "dims" : 1024
-        }
-      }
+
+index_name_desc = "embeddings-books"
+
+
+def recreateIndexDesc(vectDescBooks, index_name):
+    mappings = {
+        "properties": {"description_vector": {"type": "dense_vector", "dims": 1024}}
     }
-
-index_name = "embeddings-books"
-
-def recreateIndex( vectDescBooks, index_name):
     client.indices.delete(index=index_name)
 
-    client.indices.create(index=index_name,mappings=mappings)
+    client.indices.create(index=index_name, mappings=mappings)
 
-    for i in range(len(vectDescBooks)) :
-        doc = {
-        "description_vector" : vectDescBooks[i]
-    }
+    for i in range(len(vectDescBooks)):
+        doc = {"description_vector": vectDescBooks[i]}
         resp = client.index(index=index_name, id=i, document=doc)
-        print(resp['result'],i)
+        print(resp["result"], i)
+
+
+def recreareIndexGenre(vectGenreBooks, index_name):
+    mappings = {
+       "properties": {"genre_vector": {"type": "dense_vector", "dims": 1024}}
+    }
+    try :
+      client.indices.delete(index=index_name)
+    except Exception as e :
+      print(e)
+    
+    client.indices.create(index=index_name, mappings=mappings)
+
+    for i in range(len(vectGenreBooks)):
+        doc = {"genre_vector": vectGenreBooks[i]}
+        resp = client.index(index=index_name, id=i, document=doc)
+        print(resp["result"], i)
+
 
 # recreateIndex(vectDescBooks, index_name)
-def getvectBooks(idBook : int ) :
-    return client.get(index=index_name, id=idBook)
-
+def getvectBooks(idBook: int):
+    return client.get(index=index_name_desc, id=idBook)
