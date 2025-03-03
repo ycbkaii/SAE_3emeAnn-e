3eb3c0f801ff158f5fd091c36e0be568a6cfde29
tom27012004 <127438871+tom27012004@users.noreply.github.com>
2025-03-03 14:34:17 +0100

    Add files via upload
    Ajout algorithme recommandation

---
+++ index.py
@@ -0,0 +1,74 @@
+from fastapi import FastAPI, Path
+from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from embeddings.loadSimilarity import kNNBooks, checkClient, kNNUser
+from inputData_outputCluster import acmReco
+from fastapi.middleware.cors import CORSMiddleware
+from recommandation_aleatoire import recommend_genres
+
+
+app = FastAPI()
+
+
+# On mentionne les cors
+origins = [
+    "http://127.0.0.1:8000",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Charger le CSS et le JS dans les pages
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
+def read_root():
+    """La route par def"""
+    file_path = "Site/index.html"
+    return FileResponse(file_path)
+
+@app.get("/contact", response_class=HTMLResponse)
+def get_contact():
+    file_path = "Site/contacts.html"
+    return FileResponse(file_path)
+
+@app.get("/es_info")
+def elastic_search_info() :
+    """Renvoie les infos du clients ElasticSearch"""
+    return checkClient()
+
+
+@app.get("/books/{books_id}")
+def get_reco_books_id(books_id : int) :
+    """ Renvoie les recommandations item_base pour le livre d'id {books_id} """
+    return kNNBooks(books_id)
+
+@app.get("/user/{user_id}")
+def get_user_similar_id(user_id : int) :
+    """Renvoie les usr similaire """
+    return kNNUser(user_id)
+
+
+@app.get("/livres/acm_recom/{user_id}")
+def get_books_recom_acm(user_id : int) :
+    """Cela renvoie les la liste des livres de recommandation"""
+    return acmReco(user_id)
+
+
+@app.get("/livres/genres/{user_id}")
+def get_recommended_genres(user_id):
+    recommended = recommend_genres(user_id)
+    return recommended
+
+@app.get("/init")
+def init_ia() :
+    """Route pour init les algorithmes""" 
+    
---
+++ utilities.py
@@ -0,0 +1,30 @@
+import psycopg2
+
+
+def getBooksById(listIdBooks) :
+    
+    # Ouverture connexion bdd
+    conn = psycopg2.connect(database="masterbook",
+                        port="5433",
+                        user="root",
+                        host="localhost",
+                        password="root"
+                        )
+    cursor = conn.cursor()
+    
+    tuples = []
+    
+    for id in listIdBooks : 
+        
+        # On vérifie que l'id est un tuple ou non
+        if isinstance(id, tuple) :
+            id = id[0]
+        
+        queryToSelectBooks = f"SELECT DISTINCT(title), average_rating, nom_genre, isbn, cover_link FROM masterbook._livre NATURAL JOIN masterbook._genres_du_livre NATURAL JOIN masterbook._genre WHERE id_livre = {id};"
+        
+        cursor.execute(queryToSelectBooks)
+
+        tuples.append(cursor.fetchall()[0])
+        
+    
+    return tuples
